bf4991f935545528a0d2008e0db7e2a941febb06
silvestrevivo <silvestrevivomillan@gmail.com>
2019-11-23 20:55:19 +0100

    retrieving token from post user request

--- server.js
+++ server.js
@@ -6,12 +6,15 @@ const app = express();
 //Connect DB
 connectDB();
 
+//Init Middleware
+app.use(express.json({ extended: false }));
+
 app.get('/', (req, res) => res.json({msg: 'MERN-CONTACT-KEEPER running!!'}));
 
 //*Define routes
-app.get('/api/users', require('./routes/users'));
-app.get('/api/auth', require('./routes/auth'));
-app.get('/api/contacts', require('./routes/contacts'));
+app.use('/api/users', require('./routes/users'));
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/contacts', require('./routes/contacts'));
 
 const PORT = process.env.PORT || 5000;
 
