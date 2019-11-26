be03ec81515ecbe2f972e066ae58b7f80eff734c
silvestrevivo <silvestrevivomillan@gmail.com>
2019-11-26 11:28:42 +0100

    frontend started

---
+++ client/src/components/pages/About.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+      <h1>About ths app</h1>
+      <p>This is a full stack React app for keeping contacts</p>
+      <div className="bg-dark p">
+        <strong>Version: </strong> 1.0.0
+      </div>
+    </div>
+  )
+}
+
+export default About
