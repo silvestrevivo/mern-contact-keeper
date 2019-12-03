cd7fd078632e6fa173f4766fb15e487228076404
silvestrevivo <silvestrevivomillan@gmail.com>
2019-12-03 15:28:36 +0100

    login and register pages created

---
+++ client/src/context/auth/authReducer.js
@@ -0,0 +1,20 @@
+import {
+  REGISTER_SUCCESS,
+  REGISTER_FAIL,
+  USER_LOADED,
+  AUTH_ERROR,
+  LOGIN_SUCCESS,
+  LOGIN_FAIL,
+  LOGOUT,
+  CLEAR_ERRORS,
+} from '../types';
+
+
+export default (state, action ) => {
+  return state;
+  // switch (action.type) {
+  //     return state;
+  //   default:
+  //     return state;
+  // }
+}
