be9dbced7dd2b057ab88b8df53c8c8f5c2ade5aa
silvestrevivo <silvestrevivomillan@gmail.com>
2019-12-03 16:48:28 +0100

    alerts added to app component

--- client/src/context/types.js
+++ client/src/context/types.js
@@ -6,6 +6,7 @@ export const SET_CURRENT = 'SET_CURRENT';
 export const CLEAR_CURRENT = 'CLEAR_CURRENT';
 export const CLEAR_FILTER = 'CLEAR_FILTER';
 export const SET_ALERT = 'SET_ALERT';
+export const REMOVE_ALERT = 'REMOVE_ALERT';
 export const REGISTER_SUCCESS = 'REGISTER_SUCCESS';
 export const REGISTER_FAIL = 'REGISTER_FAIL';
 export const USER_LOADED = 'USER_LOADED';
