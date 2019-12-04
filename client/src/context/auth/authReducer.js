6dee6882a1ff3ab2f63b882ed2d9bf16237ed472
silvestrevivo <silvestrevivomillan@gmail.com>
2019-12-04 11:00:33 +0100

    registering using

--- client/src/context/auth/authReducer.js
+++ client/src/context/auth/authReducer.js
@@ -11,10 +11,31 @@ import {
 
 
 export default (state, action ) => {
-  return state;
-  // switch (action.type) {
-  //     return state;
-  //   default:
-  //     return state;
-  // }
+  switch (action.type) {
+    case REGISTER_SUCCESS:
+      localStorage.setItem('token', action.payload.token);
+      return {
+        ...state,
+        ...action.payload,
+        isAuthenticated: true,
+        loading: false
+      }
+    case REGISTER_FAIL:
+      localStorage.removeItem('token');
+      return {
+        ...state,
+        token: null,
+        isAuthenticated: false,
+        loading: false,
+        user: null,
+        error: action.payload
+      }
+    case CLEAR_ERRORS:
+      return {
+        ...state,
+        error: null
+      }
+    default:
+      return state;
+  }
 }
