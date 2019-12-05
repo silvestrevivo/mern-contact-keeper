7f058656018fb3558143c570108e5add72559243
silvestrevivo <silvestrevivomillan@gmail.com>
2019-12-05 15:53:44 +0100

    privateRoute component as HOC created

--- client/src/App.js
+++ client/src/App.js
@@ -14,6 +14,8 @@ import AlertState from './context/alert/AlertState';
 
 import setAuthToken from './utils/setAuthToken';
 
+import PrivateRoute  from './components/routing/PrivateRoute';
+
 if(localStorage.token){
   setAuthToken(localStorage.token);
 }
@@ -29,7 +31,7 @@ const App = () => {
               <div className="container">
                 <Alerts />
                 <Switch>
-                  <Route exact path="/" component={Home} />
+                  <PrivateRoute exact path="/" component={Home} />
                   <Route exact path="/about" component={About} />
                   <Route exact path="/register" component={Register} />
                   <Route exact path="/login" component={Login} />
